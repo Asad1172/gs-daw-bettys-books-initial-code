282defcffc74568a189227d6a5cf715801a83d71
Asad117 <asadullah2003@live.co.uk>
2024-10-15 12:09:17 +0100

    v4

--- create_db.sql
+++ create_db.sql
@@ -7,6 +7,21 @@ USE bettys_books;
 # Create the tables
 CREATE TABLE IF NOT EXISTS books (id INT AUTO_INCREMENT,name VARCHAR(50),price DECIMAL(5, 2) unsigned,PRIMARY KEY(id));
 
+CREATE TABLE IF NOT EXISTS passwords 
+(id INT AUTO_INCREMENT,
+firstname VARCHAR(50),
+lastname VARCHAR(50),
+username VARCHAR(50),
+email VARCHAR(70),
+hashedpassword VARCHAR(255),
+PRIMARY KEY(id));
+
 # Create the app user
 CREATE USER IF NOT EXISTS 'bettys_books_app'@'localhost' IDENTIFIED BY 'qwertyuiop'; 
 GRANT ALL PRIVILEGES ON bettys_books.* TO ' bettys_books_app'@'localhost';
+
+
+
+# Insert data into the tables
+
+USE bettys_books;
